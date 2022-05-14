63f9ab72de06e8d28921fd7dde6098be2125b282
DUBOSCLARD LÉO <leo.dubosclard@epitech.eu>
2022-05-14 15:09:55 +0200

    feat: Navbar
    Signed-off-by: DUBOSCLARD LÉO <leo.dubosclard@epitech.eu>

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import WithSubnavigation from '../components/Navbar';
+
+const Home = (): JSX.Element => {
+    return (
+        <>
+            <WithSubnavigation />
+        </>
+    );
+}
+
+export default Home;
